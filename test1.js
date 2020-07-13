4d13e5b232ea2aeb5e5fe43de3437f9eb93aa47d
Serg <Serg.wp.f5@gmail.com>
2020-07-13 18:25:21 +0300

    Test1 - 012

--- test1.js
+++ test1.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const numberOfFilms = prompt('How much movies are you watching?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    gentres: [],
+    privat: false
+};
+
+for(let i = 0; i < 2; i++) {
+
+    let movie = prompt('Last movie', '');
+    let imdb = prompt('IMDB', '');
+    personalMovieDB.movies[movie] = imdb;
+
+}
+
+console.log(personalMovieDB);
