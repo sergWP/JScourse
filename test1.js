4c4f8c02562254ffff230b4fae0d1cb2753a6988
Serg <Serg.wp.f5@gmail.com>
2020-07-17 19:39:59 +0300

    test 024

--- test1.js
+++ test1.js
@@ -1,6 +1,20 @@
 'use strict';
 
-const numberOfFilms = prompt('How much movies are you watching?', '');
+//024
+
+let numberOfFilms;
+
+// start()
+function start() {
+
+    numberOfFilms = +prompt('How much movies are you watching?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How much movies are you watching?', '');
+    }
+
+}
+start();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,43 +24,71 @@ let personalMovieDB = {
     privat: false
 };
 
-// for (let i = 0; i < 2; i++) {
-//
-//     let movie = prompt('Last movie', '');
-//     let imdb = prompt('IMDB', '');
-//
-//     if(movie && (movie.length < 50) && imdb) {
-//         personalMovieDB.movies[movie] = imdb;
-//     } else {
-//         alert('Please, enter correct data');
-//         i--;
-//     }
-//
-// }
+// rememberMyFilms()
+function rememberMyFilms() {
 
-let i = 0;
-do {
-    let movie = prompt('Last movie', '');
-    let imdb = prompt('IMDB', '');
+    for (let i = 0; i < 2; i++) {
 
-    if(movie && (movie.length < 50) && imdb) {
-        personalMovieDB.movies[movie] = imdb;
-        i++;
-    } else {
-        alert('Please, enter correct data');
+        let movie = prompt('Last movie', '');
+        let imdb = prompt('IMDB', '');
+
+        if(movie && (movie.length < 50) && imdb) {
+            personalMovieDB.movies[movie] = imdb;
+        } else {
+            alert('Please, enter correct data');
+            i--;
+        }
+
     }
 
-} while (i < 2);
-
-
-if(personalMovieDB.count <= 10) {
-    console.log('мало')
-} else if(personalMovieDB.count > 10 && personalMovieDB.count <= 30 ) {
-    console.log('норм')
-} else if(personalMovieDB.count > 30) {
-    console.log('много')
-} else {
-    console.log('err')
 }
+rememberMyFilms();
 
-console.log(personalMovieDB);
+// detectPersonalLevel()
+function detectPersonalLevel() {
+
+    if(personalMovieDB.count <= 10) {
+        console.log('мало')
+    } else if(personalMovieDB.count > 10 && personalMovieDB.count <= 30 ) {
+        console.log('норм')
+    } else if(personalMovieDB.count > 30) {
+        console.log('много')
+    } else {
+        console.log('err')
+    }
+
+}
+detectPersonalLevel();
+
+
+// showMyDB()
+function showMyDB() {
+
+    if(personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+
+}
+showMyDB();
+
+// writeYourGentres()
+
+let gentre;
+
+function writeYourGentres() {
+
+    for (let i = 1; i <=3; i++) {
+
+        gentre = prompt(`You favorite gentres number ${i}`);
+
+        if(gentre || gentre !== '' || gentre !== null) {
+            personalMovieDB.gentres[i - 1] = gentre;
+        } else {
+            alert('Please, enter correct data');
+            i--;
+        }
+
+    }
+
+}
+writeYourGentres();
